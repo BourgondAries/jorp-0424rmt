63840987e37886ef7ff5c009a38309168b082102
Bourgond Aries <macocio@gmail.com>
2016-10-14 13:40:24 +0200

    Add initial condition

--- m.m
+++ m.m
@@ -30,6 +30,8 @@ j = @(phi) [cos(phi) -sin(phi) 0 0; ...
             0        0         1 0; ...
             0        0         0 1];
 
+n0 = [0; 0; 2; 45*pi/180];
+
 surge_thrust = [-220 480];
 sway_thrust  = [-220 220];
 heave_thrust = [-180 390];
