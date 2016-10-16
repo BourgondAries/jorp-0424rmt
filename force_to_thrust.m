b8743fc82336cf0a0c5e4117704974f40a5a3aff
Bourgond Aries <macocio@gmail.com>
2016-10-16 14:20:48 +0200

    Clean up code

--- force_to_thrust.m
+++ force_to_thrust.m
@@ -1,25 +1,20 @@
 function y = dowhatiwant( U0 )
-%DOWHATIWANT Summary of this function goes here
-%   Detailed explanation goes here
-Umax = [480, 220, 390, inf]; 
-Umin = [-220, -220, -180, -inf];
-N = [0.5077 0.7646  0   -4.6062; ...
-     0.5077 -0.7646 0   4.6062; ...
-     0      1.2656  0   -1.5997; ...
-     0      0       0.5 0; ...
-     0      0       0.5 0];
-f = N*U0; 
+	Umax = [480, 220, 390, inf];
+	Umin = [-220, -220, -180, -inf];
+	N = [0.5077 0.7646  0   -4.6062; ...
+	     0.5077 -0.7646 0   4.6062; ...
+	     0      1.2656  0   -1.5997; ...
+	     0      0       0.5 0; ...
+	     0      0       0.5 0];
 
-for i = 1:3
-if f(i) < Umin(i); 
-    f(i) = Umin(i);
-elseif f(i) > Umax(i) 
-        f(i) = Umax(i);
+	f = N*U0;
+
+	for i = 1:3
+		if f(i) < Umin(i);
+			f(i) = Umin(i);
+		elseif f(i) > Umax(i)
+			f(i) = Umax(i);
+		end
+	end
+	y = f;
 end
-end
-
-
-y = f;
-
-end
-
