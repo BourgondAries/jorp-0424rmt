b70a7cbebfea8daba10803f1d3e0d38c350e49d0
Bourgond Aries <macocio@gmail.com>
2016-10-27 10:46:42 +0200

    Implement complete model

--- m.m
+++ m.m
@@ -9,34 +9,29 @@ if exist('compiled', 'var') == 0
 	compiled = true;
 end
 
-%global iM J D iMD M H B R Q Umax Umin count x_hat_0 P K T;
-
 addpath(genpath('./modelR2016bMAC/'));
 modelR2016bMAC;
 
-
-%D = [234 0   0   0; 0   292 0   0; 0   0   263 0; 0   0   0   25];
+big_delta = 0;
+big_gamma = 0;
+big_phi   = 0;
+big_c     = 0;
+big_k     = 0;
+D = [234 0   0   0; 0   292 0   0; 0   0   263 0; 0   0   0   25];
 
 % J = @(phi) [cos(phi) -sin(phi) 0 0; ...
 %             sin(phi) cos(phi)  0 0; ...
 %             0        0         1 0; ...
 %             0        0         0 1];
-%T = 0.2; 
+
+% Constant thrust given by the vessel
+thrust = [0, 0, 5, 0]';
+
+% Initial condition of the system
 Eta0 = [0; 0; 2; 45*pi/180; 0; 0]';
+
+% Various states of simulation
 CurrentEnabled    = 0;
 HiPAPpeaksEnabled = 0;
 SensNoiseEnabled  = 0;
-WavesEnabled      = 0;
-
-
-U0 = [0, 0, 5, 0]';
-deg2rad = pi/180; 
-  
-%P = eye(8); 
-%K = zeros(8, 5); 
-%x_hat_0= [0, 0, 2, 45*deg2rad, 0, 0, 0, 0]; 
-%iM = inv(M_tot);
-%iMD = iM*(-D);
-   
-%B = [zeros(4); inv(M_tot)];
-
+WavesEnabled      = 1;
