2a3f30e2e0877995dfe6d925bb6e4d3406964424
Bourgond Aries <macocio@gmail.com>
2016-10-27 12:30:15 +0200

    Add the linearized block

--- m.m
+++ m.m
@@ -42,27 +42,21 @@ Dt = 0.2;
 mass = added_mass + rigid_mass;
 big_delta = [zeros(4); Dt*(added_mass + rigid_mass)^-1; zeros(4)];
 big_omega = 2*wave_dampings.*wave_frequencies;
-big_gamma = wave_frequencies.^2;
+big_gamma = [zeros(4); wave_frequencies.^2; zeros(4)];
 
-U = 1;
-V = 1;
-W = 1;
-Phi = 0;
-
-%{
-big_phi   = [1, 0, 0, -(V*cos(Phi) + U*sin(Phi))*Dt, Dt*cos(Phi), -Dt*sin(Phi), 0, 0, 0, 0, 0, 0;
-						 0, 1, 0, (U*cos(Phi) - V*sin(Phi))*Dt, Dt*sin(Phi), Dt*cos(Phi), 0, 0, 0, 0, 0, 0;
+big_phi   = @(x) [1, 0, 0, -(x(6)*cos(x(4)) + x(5)*sin(x(4)))*Dt, Dt*cos(x(4)), -Dt*sin(x(4)), 0, 0, 0, 0, 0, 0;
+						 0, 1, 0, (x(5)*cos(x(4)) - x(6)*sin(x(4)))*Dt, Dt*sin(x(4)), Dt*cos(x(4)), 0, 0, 0, 0, 0, 0;
 						 0, 0, 1, 0, 0, 0, Dt, 0, 0, 0, 0, 0;
 						 0, 0, 0, 1, 0, 0, 0, Dt, 0, 0, 0, 0;
-						 0, 0, 0, (B2*cos(Phi) - B1*sin(Phi))*Dt/mass(1,1), -big_d(1,1)*Dt/mass(1,1) + 1, 0, 0, 0, Dt*cos(Phi)/mass(1,1), Dt*sin(Phi)/mass(1,1), 0, 0;
-						 0, 0, 0, -(B1*cos(Phi) + B2*sin(Phi))*Dt/mass(2,2), 0, -big_d(2,2)*Dt/mass(2,2) + 1, 0, 0, -Dt*sin(Phi)/mass(2,2), Dt*cos(Phi)/mass(2,2), 0, 0;
+						 0, 0, 0, (x(10)*cos(x(4)) - x(9)*sin(x(4)))*Dt/mass(1,1), -big_d(1,1)*Dt/mass(1,1) + 1, 0, 0, 0, Dt*cos(x(4))/mass(1,1), Dt*sin(x(4))/mass(1,1), 0, 0;
+						 0, 0, 0, -(x(9)*cos(x(4)) + x(10)*sin(x(4)))*Dt/mass(2,2), 0, -big_d(2,2)*Dt/mass(2,2) + 1, 0, 0, -Dt*sin(x(4))/mass(2,2), Dt*cos(x(4))/mass(2,2), 0, 0;
 						 0, 0, 0, 0, 0, 0, -big_d(3,3)*Dt/mass(3,3) + 1, 0, 0, 0, Dt/mass(3,3), 0;
 						 0, 0, 0, 0, 0, 0, 0, -big_d(4,4)*Dt/mass(4,4) + 1, 0, 0, 0, Dt/mass(4,4);
 						 0, 0, 0, 0, 0, 0, 0, 0, -Dt*bias_matrix(1,1) + 1, 0, 0, 0;
 						 0, 0, 0, 0, 0, 0, 0, 0, 0, -Dt*bias_matrix(2,2) + 1, 0, 0;
 						 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -Dt*bias_matrix(3,3) + 1, 0;
 						 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -Dt*bias_matrix(4,4) + 1]
-%}
+
 big_k     = [1 0 0 0 0
              0 1 0 0 0
              0 0 1 0 0
