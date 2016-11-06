b07145a51ad90f5d2057aab3a7149505722fc564
Stephanie <steffyquayson@hotmail.com>
2016-11-06 14:03:30 +0100

    Added a guidance system

--- m.m
+++ m.m
@@ -2,18 +2,18 @@
 clc; clearvars -except compiled; close all;
 
 if exist('compiled', 'var') == 0
-	fprintf('Mexing the subsystem\n');
-	mex ./modelR2016bMAC/Subsystem_sf.c ...
-		./modelR2016bMAC/Subsystem_sfcn_rtw/rtGetNaN.c ...
-		./modelR2016bMAC/Subsystem_sfcn_rtw/rtGetInf.c
-	compiled = true;
+    fprintf('Mexing the subsystem\n');
+    mex ./modelR2016bMAC/Subsystem_sf.c ...
+        ./modelR2016bMAC/Subsystem_sfcn_rtw/rtGetNaN.c ...
+        ./modelR2016bMAC/Subsystem_sfcn_rtw/rtGetInf.c
+    compiled = true;
 end
 
 addpath(genpath('./modelR2016bMAC/'));
 modelR2016bMAC;
 
 % Example case, decides initial condition and thrust
-model_case = 5;
+model_case = 7;
 
 % Various states of simulation
 CurrentEnabled    = 1;
@@ -21,24 +21,27 @@ HiPAPpeaksEnabled = 1;
 SensNoiseEnabled  = 1;
 WavesEnabled      = 1;
 
+% Defining constants for framsforming between radians and degrees
+deg2rad = pi/180;
+rad2deg =180/pi;
 
 % Vessel model
 Ma = [290 0   0   0;
-      0   300 0   0;
-      0   0   330 0;
-      0   0   0   55];
+    0   300 0   0;
+    0   0   330 0;
+    0   0   0   55];
 
 Mrb = [460 0   0   0;
-       0   460 0   0;
-       0   0   460 0;
-       0   0   0   105];
+    0   460 0   0;
+    0   0   460 0;
+    0   0   0   105];
 
 M = Ma + Mrb;
 
 D    = [234 0   0   0;
-        0   292 0   0;
-        0   0   263 0;
-        0   0   0   25];
+    0   292 0   0;
+    0   0   263 0;
+    0   0   0   25];
 
 g = [0,0,5,0];
 
@@ -72,22 +75,54 @@ Gpd = [Gp Gd];
 
 
 switch model_case
-	case 1
-		% Initial condition of the system
-		Eta0 = [0; 0; 2; 0; 0; 45*pi/180]';
-		% Constant thrust given by the vessel
-		u = [0, 0, 5, 0]';
-	case 2
-		Eta0 = [0; 0; 300; 0; 0; 45*pi/180]';
-		u = [300, 0, 0, 0]';
-	case 3
-		Eta0 = [0; 0; 300; 0; 0; 45*pi/180]';
-		u = [100, 0, 0, 0.2]';
+    case 1
+        % Initial condition of the system
+        Eta0 = [0; 0; 2; 0; 0; 45*pi/180]';
+        % Constant thrust given by the vessel
+        u = [0, 0, 5, 0]';
+    case 2
+        Eta0 = [0; 0; 300; 0; 0; 45*pi/180]';
+        u = [300, 0, 0, 0]';
+    case 3
+        Eta0 = [0; 0; 300; 0; 0; 45*pi/180]';
+        u = [100, 0, 0, 0.2]';
     case 4
-		Eta0 = [0; 0; 1; 0; 0; 45*pi/180]';
-		u = [0, 0, 320, 0.012]';
+        Eta0 = [0; 0; 1; 0; 0; 45*pi/180]';
+        u = [0, 0, 320, 0.012]';
+        
     case 5
         Eta0 = [0; 0; 200; 0; 0; 0;]';
+        pos.x = [Eta0(1) Eta0(1)];
+        pos.y = [Eta0(2) Eta0(2)] ;
+        pos.z = [Eta0(3) Eta0(3)];
+        pos.psi =[Eta0(6) Eta0(6)];
+        
+        
+    case 6
+        Eta0 = [0; 0; 200; 0; 0; 0;]';
+        pos.x = [Eta0(1) 0];
+        pos.y = [Eta0(2) 0] ;
+        pos.z = [Eta0(3) 200];
+        pos.psi =[Eta0(6) 320*deg2rad ];
+        
+        
+    case 7
+        
+        Eta0 = [0; 0; 200; 0; 0; 0;]';
+        pos.x = [Eta0(1) 100];
+        pos.y = [Eta0(2) 60] ;
+        pos.z = [Eta0(3) 150];
+        pos.psi =[Eta0(6) 320*deg2rad];
+        
+        
+    case 8
+        
+        Eta0 = [0; 0; 1; 0; 0; 0;]';
+        pos.x = [Eta0(1) 10];
+        pos.y = [Eta0(2) Eta0(2)] ;
+        pos.z = [Eta0(3) 5];
+        pos.psi =[Eta0(6) Eta0(6)];
+        
 end
 
 % Initial values:
@@ -96,6 +131,18 @@ P0 = blkdiag(eye(8), eye(4)*10, eye(4), eye(4));
 e0 = [0 0 0 0]';
 u0 = [0, 0, 0, 0]';
 Transf = [cos(Eta0(6)) sin(Eta0(6)) 0 0;
-         -sin(Eta0(6)) cos(Eta0(6)) 0 0;
-            0             0         1 0;
-            0             0         0 1];
+    -sin(Eta0(6)) cos(Eta0(6)) 0 0;
+    0             0         1 0;
+    0             0         0 1];
+
+% Creating the path vectors
+time = [0  240];
+step = 5;
+path = zeros(4, max(time)/step + 1); %a matrix descibing the path. First row is the time vector
+
+t = 0: step: max(time); %adding time to the path description
+path(1,:) = spline(time, pos.x, t); %path in x direction
+path(2,:) = spline(time, pos.y, t);%path in y direction
+path(3,:) = spline(time, pos.z, t);%path in z direction
+path(4,:) = spline(time, pos.psi, t);%path for the hading
+
