eba778319ab8981639238fb8b9fc6a4ff323b2b8
Stephanie <steffyquayson@hotmail.com>
2016-11-20 17:08:08 +0100

    Impl changes for setpoints

--- generatePath.m
+++ generatePath.m
@@ -3,24 +3,25 @@
 function path = generatePath(WP)
 
 path = [];
-if(size(WP, 1)) < 2 %stationkeeping
-    path = [WP(1, 1) WP(1, 2) WP(1, 3) WP(1, 4)];
+if(size(WP, 1)) < 2 %stationkeeping 
+    path = [WP(1, 1) WP(1, 2) WP(1, 3) WP(1, 4)]';
     
 elseif size(WP, 1) == 2 %Going from A to B and/or rotating 
     
     if(WP(1,1) ~= WP(2,1) | WP(1,2) ~= WP(2,2) | WP(1,3) ~= WP(2,3) ) % if translatory motion
-        points = 4;
+        points = 15;
         alpha = atan2(WP(2, 2), WP(2, 1));  %angle to final point
         north = linspace(WP(1, 1), WP(2,1), points); %crating points on the straight line
         east = linspace(WP(1, 2), WP(2,2), points); %crating points on the straight line
         down  = linspace(WP(1, 3), WP(2,3), points); %crating points on the straight line
         
         for i = 1: points
-            path(:,i) =  [north(i) east(i) down(i) alpha];
+            path(i,:) =  [north(i) east(i) down(i) alpha];
         end
         
         path(points + 1,:) = [north(points) east(points) down(points) WP(2, 4)];
         
+        path = path'; 
     else %if only rotating
         path = [WP(1, 1) WP(1, 2) WP(1, 3) WP(1, 4); WP(2, 1) WP(2, 2) WP(2, 3) WP(2, 4)]';
     end
--- plot_trajectory.m
+++ plot_trajectory.m
@@ -15,7 +15,7 @@ set(gca, 'zdir', 'reverse');
 grid
 hold on
 a = plot3(y.data(1,:), y.data(2,:), y.data(3,:), 'r') ; %Plotting Minervas trajecory
-b = plot3(setpoints(:,1), setpoints(:,2), setpoints(:,3), 'b'); % Plotting the desired path
+b = plot3(setpoints(1,:), setpoints(2,:), setpoints(3,:), 'b'); % Plotting the desired path
 plot3(WP(:,1), WP(:,2), WP(:,3), 'x'); %Marking the waypoints
 
 legend([a b],{'Real trajectory','Desiered Path'}); 
